b32d97ff832ae0db9037039091e09f63af44cfbc
Ley <joley@teksystems.com>
2023-02-22 14:29:25 -0700

    Web IT: Added Spring Boot boilerplate code

---
+++ employee-management-api/src/main/java/com/independentproject/employeemanagementapi/EmployeeManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.independentproject.employeemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApiApplication.class, args);
+    }
+
+}
