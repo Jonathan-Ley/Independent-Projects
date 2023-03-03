20ae08dcc918ddc336646b66be3f0e3aa2419bcf
Ley <joley@teksystems.com>
2023-03-03 09:15:58 -0700

    Added center section of navbar; included icons for other pages and activities

--- social-media-site/frontend-ui/client-facing/components/Header.js
+++ social-media-site/frontend-ui/client-facing/components/Header.js
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import React from "react";
-import { BiSearchAlt2 } from "react-icons/bi";
+import { BiSearchAlt2, BiHome } from "react-icons/bi";
+import { RiFlag2Line } from "react-icons/ri";
+import { MdOutlineOndemandVideo } from "react-icons/md";
+import { AiOutlineShopping } from "react-icons/ai";
+import { IoGameControllerOutline } from "react-icons/io5";
 
 const Header = () => {
   return (
@@ -22,6 +26,25 @@ const Header = () => {
         </div>
       </div>
       {/* center component */}
+      <div className="flex flex-grow justify-center mx-2">
+        <div className="flex items-center">
+          <div className="flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-200 cursor-pointer">
+            <BiHome className="mx-auto" size={25} />
+          </div>
+          <div className="flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-200 cursor-pointer">
+            <RiFlag2Line className="mx-auto" size={25} />
+          </div>
+          <div className="flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-200 cursor-pointer">
+            <MdOutlineOndemandVideo className="mx-auto" size={25} />
+          </div>
+          <div className="flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-200 cursor-pointer">
+            <AiOutlineShopping className="mx-auto" size={25} />
+          </div>
+          <div className="flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-200 cursor-pointer">
+            <IoGameControllerOutline className="mx-auto" size={25} />
+          </div>
+        </div>
+      </div>
       {/* right component */}
     </div>
   );
