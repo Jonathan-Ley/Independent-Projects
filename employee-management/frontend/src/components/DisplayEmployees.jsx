c0249efb2ada7d107a2013f8653e7ebec03db370
Ley <joley@teksystems.com>
2023-03-14 09:36:46 -0600

    Implemented getEmployees service on frontend with useEffect

--- employee-management/frontend/src/components/DisplayEmployees.jsx
+++ employee-management/frontend/src/components/DisplayEmployees.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import EmployeeService from "../services/EmployeeService";
 
 const DisplayEmployees = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true); //used to handle loading & check if data is loaded
+  const [employees, setEmployees] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await EmployeeService.getEmployees();
+        setEmployees(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="container mx-auto my-8">
@@ -32,32 +50,43 @@ const DisplayEmployees = () => {
                 </th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <td className="text-left px-6 py-4 whitespace-normal">
-                  <div className="text-md text-gray-700">Jonathan</div>
-                </td>
-                <td className="text-left px-6 py-4 whitespace-normal">
-                  <div className="text-md text-gray-700">Ley</div>
-                </td>
-                <td className="text-left px-6 py-4 whitespace-normal">
-                  <div className="text-md text-gray-700">
-                    joley@teksystems.com
-                  </div>
-                </td>
-                <td className="text-right px-6 py-4 whitespace-normal font-bold">
-                  <a
-                    href="#"
-                    className="text-yellow-600 hover:text-indigo-800 px-4"
-                  >
-                    Update
-                  </a>
-                  <a href="#" className="text-red-600 hover:text-indigo-800">
-                    Delete
-                  </a>
-                </td>
-              </tr>
-            </tbody>
+            {!loading && (
+              <tbody>
+                {employees.map((employee) => (
+                  <tr key={employee.id}>
+                    <td className="text-left px-6 py-4 whitespace-normal">
+                      <div className="text-md text-gray-700">
+                        {employee.firstName}
+                      </div>
+                    </td>
+                    <td className="text-left px-6 py-4 whitespace-normal">
+                      <div className="text-md text-gray-700">
+                        {employee.lastName}
+                      </div>
+                    </td>
+                    <td className="text-left px-6 py-4 whitespace-normal">
+                      <div className="text-md text-gray-700">
+                        {employee.emailId}
+                      </div>
+                    </td>
+                    <td className="text-right px-6 py-4 whitespace-normal font-bold">
+                      <a
+                        href="#"
+                        className="text-yellow-600 hover:text-indigo-800 px-4"
+                      >
+                        Update
+                      </a>
+                      <a
+                        href="#"
+                        className="text-red-600 hover:text-indigo-800"
+                      >
+                        Delete
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            )}
           </table>
         </div>
       </div>
