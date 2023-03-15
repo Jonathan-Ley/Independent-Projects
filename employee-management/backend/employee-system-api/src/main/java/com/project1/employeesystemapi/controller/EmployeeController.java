f97545a35118942c33557bf0e1e0e54c3b73c875
Ley <joley@teksystems.com>
2023-03-15 09:00:09 -0600

    Began implementing the delete api for the service

--- employee-management/backend/employee-system-api/src/main/java/com/project1/employeesystemapi/controller/EmployeeController.java
+++ employee-management/backend/employee-system-api/src/main/java/com/project1/employeesystemapi/controller/EmployeeController.java
@@ -4,9 +4,12 @@ package com.project1.employeesystemapi.controller;
 import com.project1.employeesystemapi.model.Employee;
 import com.project1.employeesystemapi.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -28,5 +31,14 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+        boolean deleted = false;
+        deleted = employeeService.deleteEmployee(id);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted",deleted);
+        return ResponseEntity.ok(response);
     }
 }
