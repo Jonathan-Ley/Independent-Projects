83a9280b3b6e0f22f218a927a60ead21bc6f4f6b
Ley <joley@teksystems.com>
2023-04-07 09:38:11 -0600

    Learned about intrinsic cloudformation functions

--- cdk-practice/lib/PhotosStack.ts
+++ cdk-practice/lib/PhotosStack.ts
@@ -1,8 +1,21 @@
 import * as cdk from "aws-cdk-lib";
+import { Fn } from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export class PhotosStack extends cdk.Stack {
+  private stackSuffix: string;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    this.initializeSuffix();
+    new Bucket(this, "PhotosBucket", {
+      bucketName: `photos-bucket-${this.stackSuffix}`,
+    });
+  }
+
+  private initializeSuffix() {
+    const shortStackId = cdk.Fn.select(2, Fn.split("/", this.stackId));
+    this.stackSuffix = cdk.Fn.select(4, Fn.split("-", shortStackId));
   }
 }
